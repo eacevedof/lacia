166618068edd75fe9792cc7ec6da0f80ea638b62
ioedumac <eacevedof@yahoo.es>
2022-10-28 21:11:35 +0200

    be test ok bearer token

--- apps/lumen/app/Providers/Spotify/SpotifyConnector.php
+++ apps/lumen/app/Providers/Spotify/SpotifyConnector.php
@@ -2,7 +2,32 @@
 
 namespace App\Providers\Spotify;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
 final class SpotifyConnector
 {
+    private const CACHE_KEY = "spotify-bearer-token";
 
+    private string $clientId;
+    private string $clientSecret;
+
+    public function __construct()
+    {
+        $this->clientId = env("SPOTIFY_CLIENT_ID");
+        $this->clientSecret = env("SPOTIFY_CLIENT_SECRET");
+    }
+
+    public function getBearerToken(): array
+    {
+        $response = Http::withHeaders([
+            "Authorization" => "Basic ".base64_encode($this->clientId.":".$this->clientSecret)
+        ])->asForm()
+            ->post("https://accounts.spotify.com/api/token", [
+                "grant_type" => "client_credentials",
+            ]);
+
+        //print_r($response->json());
+        return [];
+    }
 }
--- apps/lumen/tests/SpotifyConnectorTest.php
+++ apps/lumen/tests/SpotifyConnectorTest.php
@@ -7,7 +7,14 @@ final class SpotifyConnectorTest extends TestCase
 {
     public function test_connector()
     {
-        $conector = new SpotifyConnector();
-        $this->assertInstanceOf(SpotifyConnector::class, $conector);
+        $connector = new SpotifyConnector();
+        $this->assertInstanceOf(SpotifyConnector::class, $connector);
+    }
+
+    public function test_nok_bearer_token()
+    {
+        $connector = new SpotifyConnector();
+        $bearerToken = $connector->getBearerToken();
+        $this->assertArrayNotHasKey("access_token", $bearerToken);
     }
 }
