e3a3b5ee0ef3bce7cbe04d47c7ec3a603c543157
ioedumac <eacevedof@yahoo.es>
2022-10-29 18:27:27 +0200

    be search transformed test ok

--- apps/lumen/tests/SearchTransformerTest.php
+++ apps/lumen/tests/SearchTransformerTest.php
@@ -21,52 +21,15 @@ final class SearchTransformerTest extends TestCase
     ];
 
     private $albums;
-    private $anyResult;
+    private $anyTransformResult;
 
     public function setUp(): void
     {
         $provider = new SpotifyAlbumsProvider(new SpotifyConnector());
         $this->albums = $provider->getAlbumsOrFail(self::BAND_NAME);
-        $this->anyResult = (new SearchTransformer())->transformAlbums($this->albums);
+        $this->anyTransformResult = (new SearchTransformer())->transformAlbums($this->albums);
     }
 
-    /*
-     *   /*
-     *
-        return $album ? [
-            "name" => $album["name"] ?? "",
-            "released" => $album["release_date"] ?? "",
-            "tracks" => $album["total_tracks"] ?? "",
-            "cover" => $this->transformCover($album["images"][0] ?? []),
-        ] : [];
-
-            return $image ? [
-            "height" => $image["height"] ?? "",
-            "width" => $image["width"] ?? "",
-            "url" => $image["url"] ?? "",
-        ]: [];
-
-    objeto saliente
-
-        name:
-        released
-        tracks:
-        cover {
-            heigh
-            widh
-            url
-        }
-  {
-    "name": "Gira Me Verás Volver",
-    "released": "2008-07-14",
-    "tracks": 14,
-    "cover": {
-      "height": 640,
-      "width": 640,
-      "url": "https://i.scdn.co/image/ab67616d0000b27352cd84d57eb20d5e477a94a9"
-    }
-  },
-     * */
     public function test_albums_is_an_array()
     {
         $this->assertIsArray($this->albums);
@@ -87,22 +50,31 @@ final class SearchTransformerTest extends TestCase
             if (count($image0)!==count($reqImg0))
                 $this->fail("not required nodes in album image0 $i");
         }
+
         $this->assertTrue(count($this->albums)===($i+1));
     }
 
     public function test_transformed_result_is_an_array()
     {
-        $this->assertIsArray($this->anyResult);
+        $this->assertIsArray($this->anyTransformResult);
     }
 
-    public function est_transformed_result_with_root_nodes()
+    public function test_required_nodes_in_transformed_array()
     {
+        $reqRoot = $this->requiredNodes["result"]["root"];
+        $reqCover = $this->requiredNodes["result"]["cover"];
 
+        foreach ($this->anyTransformResult as $i => $album) {
+            $root = array_keys($album);
+            $root = array_intersect($reqRoot, $root);
+            if (count($root)!==count($reqRoot))
+                $this->fail("not required nodes in transformed root $i");
+
+            $cover = array_keys($album["cover"]);
+            $cover = array_intersect($reqCover, $cover);
+            if (count($cover)!==count($reqCover))
+                $this->fail("not required nodes in transformed cover $i");
+        }
+        $this->assertTrue(count($this->anyTransformResult)===($i+1));
     }
-
-    public function est_transformed_result_has_all_required_nodes()
-    {
-
-    }
-
 }
