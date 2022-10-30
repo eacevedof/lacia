08dd5787bfc3aeea4b461979a201913d5535ff6b
ioedumac <eacevedof@yahoo.es>
2022-10-30 12:47:50 +0100

    fe me esta dando error la redireccion con auth0

--- apps/vue/src/App.vue
+++ apps/vue/src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/movies">Movies</router-link>
+    <router-link to="/logout">Log out</router-link>
   </nav>
   <router-view/>
 </template>
--- apps/vue/src/views/HomeView.vue
+++ apps/vue/src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <WellcomeComp msg="Welcome to Your Vue.js App"/>
+    <wellcome-comp msg="Welcome to Your Vue.js App"/>
+    <login-comp />
+    <logout-comp />
   </div>
 </template>
 
 <script>
 import WellcomeComp from "@/components/WelcomeComp.vue"
+import LoginComp from "@/components/LoginComp"
+import LogoutComp from "@/components/LogoutComp"
 
 export default {
   name: "HomeView",
   components: {
-    WellcomeComp
+    WellcomeComp,
+    LoginComp,
+    LogoutComp,
   }
 }
 </script>
