082fd1a5659826ab4758ce9928d64c5d93308336
ioedumac <eacevedof@yahoo.es>
2022-10-30 13:16:38 +0100

    fe libs

---
+++ apps/vue/src/libs/providers/movies/movies-provider.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const ENDPOINT = process.env.VUE_APP_MOVIES_ENDPOINT
+
+export const async_find_all = async () => {
+    try {
+        const movies = await axios.get(ENDPOINT)
+        return movies.data
+    } catch (err) {
+        console.log("movies async find all error")
+    }
+}
