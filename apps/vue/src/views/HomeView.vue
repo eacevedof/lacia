e3ff8678d2756dce1df53e79d3153ff6d87b6602
ioedumac <eacevedof@yahoo.es>
2022-10-30 18:45:03 +0100

    fe limpiando comps

--- apps/vue/src/views/HomeView.vue
+++ apps/vue/src/views/HomeView.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <wellcome-comp msg="Welcome to Your Vue.js App"/>
-    <login-comp />
-    <logout-comp />
+    <img alt="Vue logo" src="../assets/logo.png" height="300" width="300">
+    <wellcome-comp msg="Welcome to Movies App"/>
   </div>
 </template>
 
 <script>
 import WellcomeComp from "@/components/WelcomeComp.vue"
-import LoginComp from "@/components/LoginComp"
-import LogoutComp from "@/components/LogoutComp"
 
 export default {
   name: "HomeView",
   components: {
     WellcomeComp,
-    LoginComp,
-    LogoutComp,
   }
 }
 </script>
