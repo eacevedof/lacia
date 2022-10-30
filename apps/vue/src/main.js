41b39c7396c2033958b622ea199826aab4c2ec41
ioedumac <eacevedof@yahoo.es>
2022-10-30 11:57:05 +0100

    fe instalacion de axios y prueba get da error CORS

--- apps/vue/src/main.js
+++ apps/vue/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+
 
 console.log("ENV VARS:",process.env)
+axios.get(process.env.VUE_APP_MOVIES_ENDPOINT).then( result => console.log(result.data))
+
 createApp(App).use(store).use(router).mount('#app')
