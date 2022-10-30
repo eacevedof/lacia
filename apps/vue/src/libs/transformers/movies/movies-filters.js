90ce33af5200b89bf1202d7d0fbb73832d6b03aa
ioedumac <eacevedof@yahoo.es>
2022-10-30 15:01:21 +0100

    transformer movies fileter

--- apps/vue/src/libs/transformers/movies/movies-filters.js
+++ apps/vue/src/libs/transformers/movies/movies-filters.js
@@ -1,22 +1,40 @@
-let movies = []
+const get_distinct_years = movies => Array.from(new Set(movies.map(movie => movie?.releaseYear ?? 0))).sort().map(year => ({key:year,value:year}))
 
-
-const get_distinct_years = () => Array.from(new Set(movies.map(movie => movie?.releaseYear ?? 0))).sort()
-
-const get_distinct_types = () => Array.from(new Set(movies.map(movie => movie?.programType ?? 0))).sort()
+const get_distinct_types = movies => Array.from(new Set(movies.map(movie => movie?.programType ?? 0))).sort().map(type => ({key:type,value:type}))
 
 const get_orderby_values = () => [{key:"name",value:"Name"}, {key:"year",value:"Year"}]
-    // Se podrá ordenar por: nombre, año.
-    // Se podrá filtrar por: año, película o serie.
+const get_filtered_by_type = (type, movs) => movs.filter(movie => movie?.programType === type)
+const get_filtered_by_year = (year, movs) => movs.filter(movie => movie?.releaseYear === year)
+
+const _order_by_name = (moviea, movieb) => {
+    if (moviea.title > movieb.title) return 1
+    if (moviea.title < movieb.title) return -1
+    return 0
+}
+
+const _order_by_year = (moviea, movieb) => {
+    const yearA = parseInt(moviea?.releaseYear) ?? 0
+    const yearB = parseInt(movieb?.releaseYear) ?? 0
+    if (yearA > yearB) return 1
+    if (yearA < yearB) return -1
+    return 0
+}
+
+const get_ordered_by_year = (year, movs) => movs.sort(_order_by_year)
+const get_ordered_by_name = (name, movs) => movs.sort(_order_by_name)
 
 
-const movieTransform  = {
+const filters  = {
+    //caga de selectores
     get_distinct_years,
     get_distinct_types,
     get_orderby_values,
 
+    //aplicacion de filtros
+    get_filtered_by_year,
+    get_filtered_by_type,
+    get_ordered_by_name,
+    get_ordered_by_year
 }
 
-export const setMovies = movs => movies = movs
-
-export default movieTransform
+export default filters
