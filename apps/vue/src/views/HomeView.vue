ad274dda7c5568b092b13b4698c14d859c049930
ioedumac <eacevedof@yahoo.es>
2022-10-30 11:50:51 +0100

    fe refacotr comp

--- apps/vue/src/views/HomeView.vue
+++ apps/vue/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WellcomeComp msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WellcomeComp from "@/components/WelcomeComp.vue"
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
+    WellcomeComp
   }
 }
 </script>
