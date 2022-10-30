572e81af0fcf0ffa2b5da38d23b74c66bd64d375
ioedumac <eacevedof@yahoo.es>
2022-10-30 12:35:43 +0100

    fe config createAuth0 y quito prueba axios

--- apps/vue/src/main.js
+++ apps/vue/src/main.js
@@ -2,10 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from "axios"
+//import axios from "axios"
+import {createAuth0} from "@auth0/auth0-vue"
 
+const ENV = process.env
+//console.log("ENV VARS:",ENV)
+//axios.get(process.env.VUE_APP_MOVIES_ENDPOINT).then( result => console.log(result.data))
 
-console.log("ENV VARS:",process.env)
-axios.get(process.env.VUE_APP_MOVIES_ENDPOINT).then( result => console.log(result.data))
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).
+    use(store).
+    use(router).
+    //use(axios). //da error de recursividad
+    use(createAuth0({
+        domain: ENV.VUE_APP_AUTH0_DOMAIN,
+        client_id: ENV.VUE_APP_AUTH0_DOMAIN,
+        redirect_uri: window.location.origin,
+    })).
+    mount('#app')
